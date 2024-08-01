d0539b31440b8171e497f4bbc8082a1e3536e76e
thanhtuyenne <88424555+thanhtuyenne@users.noreply.github.com>
2024-08-01 22:22:55 +0700

    first commit

---
+++ src/client/components/CreateTodoForm.tsx
@@ -0,0 +1,100 @@
+import { useState } from 'react'
+
+import { api } from '@/utils/client/api'
+
+/**
+ * QUESTION 1:
+ * -----------
+ * Style the "Add" button so that it looks like the design in Figma.
+ *
+ * NOTE: You must use tailwindcss and className. Do not use other methods (eg.
+ * inline styles, separate css files, css modules, etc.) unless absolutely
+ * necessary. This applies to all styling-related questions in this assignment.
+ *
+ * Documentation references:
+ *  - https://tailwindcss.com
+ *  - https://www.youtube.com/watch?v=mr15Xzb1Ook
+ *
+ *
+ *
+ * QUESTION 2:
+ * -----------
+ * Currently our form is not keyboard accessible. Users cannot hit
+ * <Enter> right after typing to submit the form (add new todo). Fix this issue.
+ */
+
+export const CreateTodoForm = () => {
+  const [todoBody, setTodoBody] = useState('')
+
+  const apiContext = api.useContext()
+
+  const { mutate: createTodo, isLoading: isCreatingTodo } =
+    api.todo.create.useMutation({
+      onSuccess: () => {
+        apiContext.todo.getAll.refetch()
+      },
+    })
+
+  const [isError, setIsError] = useState(false)
+
+  return (
+    <>
+      <form className="group flex items-center justify-between rounded-12 border border-gray-200 py-2 pr-4 focus-within:border-gray-400">
+        <label htmlFor={TODO_INPUT_ID} className="sr-only">
+          Add todo
+        </label>
+
+        <input
+          id={TODO_INPUT_ID}
+          type="text"
+          placeholder="Add todo"
+          value={todoBody}
+          onChange={(e) => {
+            setTodoBody(e.target.value)
+          }}
+          onKeyDown={(ev) => {
+            if (ev.key === 'Enter') {
+              ev.preventDefault()
+              if (todoBody === '') {
+                setIsError(true)
+              } else {
+                createTodo({
+                  body: todoBody,
+                })
+                setTodoBody('')
+                setIsError(false)
+              }
+            }
+          }}
+          className="flex-1 px-4 text-base placeholder:text-gray-400 focus:outline-none"
+        />
+        <button
+          type="button"
+          disabled={isCreatingTodo && isError}
+          onClick={() => {
+            if (todoBody === '') {
+              setIsError(true)
+            } else {
+              createTodo({
+                body: todoBody,
+              })
+              setIsError(false)
+            }
+            setTodoBody('')
+          }}
+          className="rounded-[1rem] bg-gray-700 px-5 py-1 text-white"
+          id="addButton"
+        >
+          Add
+        </button>
+      </form>
+      {isError && (
+        <p className="pl-2 pt-2 text-sm font-bold text-[#e74c3c]">
+          Please enter your task
+        </p>
+      )}
+    </>
+  )
+}
+
+const TODO_INPUT_ID = 'todo-input-id'
---
+++ src/pages/index.tsx
@@ -0,0 +1,71 @@
+import * as Tabs from '@radix-ui/react-tabs'
+import { useState } from 'react'
+
+import { CreateTodoForm } from '@/client/components/CreateTodoForm'
+import { TodoList } from '@/client/components/TodoList'
+/**
+ * QUESTION 6:
+ * -----------
+ * Implement quick filter/tab feature so that we can quickly find todos with
+ * different statuses ("pending", "completed", or both). The UI should look like
+ * the design on Figma.
+ *
+ * NOTE:
+ *  - For this question, you must use RadixUI Tabs component. Its Documentation
+ *  is linked below.
+ *
+ * Documentation references:
+ *  - https://www.radix-ui.com/docs/primitives/components/tabs
+ */
+
+const Index = () => {
+  const [currentStatus, setCurrentStatus] = useState('all')
+  return (
+    <main className="mx-auto w-[480px] pt-12">
+      <div className="rounded-12 bg-white p-8 shadow-sm">
+        <h1 className="text-center text-4xl font-extrabold text-gray-900">
+          Todo App
+        </h1>
+
+        {/* TABS */}
+        <Tabs.Root
+          className="TabsRoot"
+          defaultValue="all"
+          onValueChange={(val) => setCurrentStatus(val)}
+        >
+          <Tabs.List className="TabsList flex gap-4 py-6">
+            <Tabs.Trigger
+              className="TabsTrigger rounded-[1rem] border border-[#b2bec3] px-4 py-1.5 text-sm font-bold text-[gray-700] transition-[0.25s] data-[state=active]:border-gray-700 data-[state=active]:bg-gray-700 data-[state=active]:text-white"
+              value="all"
+            >
+              All
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              className="TabsTrigger rounded-[1rem] border border-[#b2bec3] px-4 py-1.5 text-sm font-bold text-[gray-700] transition-[0.25s] data-[state=active]:border-gray-700 data-[state=active]:bg-gray-700 data-[state=active]:text-white"
+              value="pending"
+            >
+              Pending
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              className="TabsTrigger rounded-[1rem] border border-[#b2bec3] px-4 py-1.5 text-sm font-bold text-[gray-700] transition-[0.25s] data-[state=active]:border-gray-700 data-[state=active]:bg-gray-700 data-[state=active]:text-white"
+              value="completed"
+            >
+              Completed
+            </Tabs.Trigger>
+          </Tabs.List>
+          <Tabs.Content className="TabsContent" value={currentStatus}>
+            <div className="pt-10">
+              <TodoList status={currentStatus} />
+            </div>
+          </Tabs.Content>
+        </Tabs.Root>
+
+        <div className="pt-10">
+          <CreateTodoForm />
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Index
